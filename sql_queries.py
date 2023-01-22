706a19fb16df26e12768d886bfcf81262e13fa8f
Cian Young <6599253+cianyoung@users.noreply.github.com>
2023-01-21 19:31:27 -0800

    sql_queries.py string constants

--- sql_queries.py
+++ sql_queries.py
@@ -1,3 +1,6 @@
+"""Module contains string constants which contain SQL scripts for creating and dropping tables as well as inserting data into tables.
+"""
+
 import configparser
 
 
@@ -9,33 +12,76 @@ config.read('dwh.cfg')
 
 staging_events_table_drop = ""
 staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS us
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id SERIAL PRIMARY KEY,
+    start_time timestamp NOT NULL REFERENCES time(start_time),
+    user_id INT NOT NULL REFERENCES users(user_id),
+    level varchar NOT NULL,
+    song_id varchar REFERENCES songs(song_id),
+    artist_id varchar REFERENCES artists(artist_id),
+    session_id INT NOT NULL,
+    location text,
+    user_agent text
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR,
+    level VARCHAR NOT NULL,
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR PRIMARY KEY,
+    title TEXT NOT NULL,
+    artist_id VARCHAR REFERENCES artist(artist_id),
+    year INT NOT NULL,
+    duration numeric NOT NULL,
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    location VARCHAR,
+    lattitude VARCHAR,
+    longitude VARCHAR
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP PRIMARY KEY,
+    hour INT NOT NULL,
+    day INT NOT NULL,
+    week INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    weekday int NOT NULL
+);
 """)
 
 # STAGING TABLES
